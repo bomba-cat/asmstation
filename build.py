7d2c6a3acd0c96918ccff71d4d907d69b22b13f7
xk-rl <bledion.morina@hotmail.com>
2024-03-10 13:15:09 +0100

    build.py, ebr, oem

--- build.py
+++ build.py
@@ -1,3 +1,12 @@
 import os as linux
 
+commands = [
+    "nasm -f bin ./src/bootloader/bootloader.asm -o ./build/binary/bootloader.bin",
+    "dd if=/dev/zero of=./build/asmstation.img bs=1024 count=1440",
+    "dd if=./build/binary/bootloader.bin of=./build/asmstation.img seek=0 conv=notrunc",
+]
 
+for command in commands:
+    stat = linux.popen(command).read()
+    if stat != "":
+        print(stat)
--- src/bootloader/bootloader.asm
+++ src/bootloader/bootloader.asm
@@ -1,16 +1,78 @@
         org 0x7C00
         bits 16
 
-start:
-        ;Setup stack
-        mov ax, 0
-        mov ss, ax
-        mov sp, 0x7C00
+        jmp short start                 ; Jump to the main instruction
+        nop
 
-        ;Setup data segments
+;--------------------------------------------------------------------------
+bdb_oem:
+        db 'MSWIN4.1'	                ; 8 bytes
+
+bdb_bytes_per_sector:
+        dw 512
+
+bdb_sectors_per_cluster:
+        db 1
+
+bdb_reserved_sectors:
+        dw 1
+
+bdb_fat_count:
+        db 2
+
+bdb_dir_entry_count:
+        dw 0E0h
+
+bdb_total_sectors:
+        dw 2880	                        ; 2880 * 512 = 1.44Mb
+
+bdb_media_descriptor_type:
+        db 0F0h		                    ; F0 = 3.5" Floppy disk
+
+bdb_sectors_per_fat:            
+        dw 9		                    ; 9 Sectors Fat
+
+bdb_sectors_per_track:          
+        dw 18
+
+bdb_head:                       
+        dw 2
+
+bdb_hidden_sectors:             
+        dd 0
+
+bdb_large_sector_count:         
+        dd 0
+;---------------------------------------
+;----------------Extended-Boot-Record---
+ebr_drive_number:               
+        db 0                            ; Floppy
+        db 0	    	                ; Reserved
+
+ebr_signature:                  
+        db 29h
+
+ebr_volume_id:                  
+        db 12h, 34h, 56h 78h            ; Volume ID can be anything
+
+ebr_volume_label:              
+        db 'asmstation '                ; 11 bytes, padded with spaces
+
+ebr_system_id:                  
+        db 'FAT12   '	                ; 8 bytes, padded with spaces
+;----------------------------------------------------------------------------------------
+
+start:
+        jmp setup
+setup:
+        ;Setup data segment
         mov ax, 0
         mov ds, ax
         mov es, ax
+
+        ;Setup stack
+        mov ss, ax
+        mov sp, 0x7C00
 
         ;Call main
         call main
@@ -27,5 +89,5 @@ main:
         int 0x13
         ret
 
-        times 510 - ($-$$) db 0
-        db 0x55AA
+times 510-($-$$) db 0
+dw 0x55AA
